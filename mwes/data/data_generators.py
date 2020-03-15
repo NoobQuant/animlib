f99cca36ca91618c42f2c1faa48cd1360f3eb972
ville_home <ville.v.voutilainen@gmail.com>
2020-03-15 15:34:45 +0200

    added lines to mwe_plot and corresp. datas

--- mwes/data/data_generators.py
+++ mwes/data/data_generators.py
@@ -10,14 +10,9 @@ df = pd.DataFrame({'x':[100,900,1000,1000], 'y':[100,900,900,300]})
 WriteDfToJsonVar(df,'mwes/data/path_data_pathmwe.json',"sepx")
 
 #%%
-# Create data for arrow in mwe_plot.html
-
-# Common x axis
-df = pd.DataFrame({'x':range(1,9),'y1':[2,4,4,4,4,8,4,4], 'y2':[1,2,2,3,3,2,2,1]})
-WriteDfToJsonVar(df,'mwes/data/path_data_plotmwe.json',"comx")
-
-## Separate x axes
-#df = pd.DataFrame({'x1':range(1,9),'y1':[2,4,4,4,4,8,4,4],'x2':range(8), 'y2':[1,2,2,3,3,2,2,1]})
-#df = df[['x1','y1','x2','y2']]
-#WriteDfToJsonVar(df,'mwes/data/path_data_plotmwe.json',"sepx")
-
+# Create data for lines in mwe_plot, with separate axes
+df = pd.DataFrame({'x1':range(1,9),   'y1' : [2,4,4,4,4,8,4,4],
+                   'x2':range(4,12),  'y2' : [1,2,2,3,3,2,2,1],
+                   'x3':range(8,16),  'y3' : [4,7,4,5,4,2,2,1],                   
+})
+WriteDfToJsonVar(df,'mwes/data/path_data_plotmwe.json',"sepx")
