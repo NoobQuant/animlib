23e4407473c596cd193f2cd956d95be07add67df
ville_home <ville.v.voutilainen@gmail.com>
2021-05-14 16:16:56 +0300

    record.js viewport

--- src/recording/record.js
+++ src/recording/record.js
@@ -17,7 +17,7 @@ const puppeteer = require(args[3])
   await page.goto(scenename)
 
   await sleep(4000) // wait for data to load
-  for (let frame of [...Array(seconds*60).keys()]){    
+  for (let frame of [...Array(seconds*60).keys()]){
     
     // Evaluate page
     await page.evaluate((frame) => {currentTime = frame * 1000 / 60}, frame)
@@ -30,8 +30,10 @@ const puppeteer = require(args[3])
 
     // Save screenshot
     let path = pngfolder + frame.toString().padStart(5, '0') + '.png'
-    let clip = {x: 10, y: 10, width: 1920, height: 1080}        
-    await page.screenshot({path, clip})
+    let clip = {x: 10, y: 10, width: 1920, height: 1080}
+    // If page gets cliiped, try following
+    //await page.setViewport({"width":1930, "height":1090})
+    await page.screenshot({path:path, clip:clip})
   }
   browser.close()
 })()
