f0d022ab7f0a77ba7665f1baf5a0514d717072f8
ville_home <ville.v.voutilainen@gmail.com>
2021-05-14 16:18:31 +0300

    updated WriteScatter

--- src/dataprep/WriteDfToJsonVar.py
+++ src/dataprep/WriteDfToJsonVar.py
@@ -13,7 +13,7 @@ def WriteDfToJsonVar(dff,path,orientation,varname="lineDataObject"):
         import pandas as pd
         class MyException(Exception):
                 pass
-        json = '{'        
+        json = '{'
         if orientation == 'sepx':
                 for colpairno in range(int(len(dff.columns)/2)):
                         colloc = colpairno * 2
@@ -36,32 +36,31 @@ def WriteDfToJsonVar(dff,path,orientation,varname="lineDataObject"):
                         if colloc != len(dff.columns)-1:
                                 json = json + '"'+str(seriesname)+'":' + json_p + ', '
                         else:
-                                json = json + '"'+str(seriesname)+'":' + json_p + '}'                        
+                                json = json + '"'+str(seriesname)+'":' + json_p + '}'
         
         json = "let " + varname + " = " + json
         with open(path, 'w') as outfile:
                 outfile.write(json)
 
-def WriteScatter(dff, path, varname, x_col, y_col):
+def WriteScatter(df, path, varname, x, y):
         """
-        Write scatter data from pandas dataframe to json object to be used in scatter
-        plots in animlib. Dataframe needs to have columun x_col, y_col, "color", and "r".
+        Write scatter data from pandas dataframe to json object to be used in
+        scatter plots in animlib. Dataframe needs to have columun x, y, "color",
+        and "r".
         """
         json = '['
-        counter = 0
-        for index, row in dff.iterrows():
-                counter += 1
-                json_p = '{' + \
-                        '"'+"x"+'":' + str(row[x_col]) + ',' + \
-                        '"'+"y"+'":' + str(row[y_col]) + ',' + \
-                        '"'+"color"+'":' + '"' + str(row["color"]) + '"' + ',' + \
-                        '"'+"r"+'":' + str(row["r"]) + \
-                        '}'
-                if counter != len(dff):
+        #counter = 0
+        for counter, (index, row) in enumerate(df.iterrows()):
+                #counter += 1
+                #json_p = '{' + row.to_json + '}'
+                json_p = row.to_json()
+                
+                if counter < (len(df)-1):
                         json_p = json_p + ', '
-                json = json + json_p 
+                json = json + json_p
+
         json = json + ']'
         json = "let " + varname + " = " + json
 
         with open(path, 'w') as outfile:
-                outfile.write(json)        
+                outfile.write(json)
