0458216f2d614f3de815e409fe4240f07da994ed
ville_home <ville.v.voutilainen@gmail.com>
2021-09-13 15:17:25 +0300

    Image class draft inheriting AnimObject

--- src/classes/Image.js
+++ src/classes/Image.js
@@ -1,82 +1,28 @@
-export class Image{
-/* 
-Display images from .svg or .png/jpg files.
+import {AnimObject} from './AnimObject.js'
+export class Image extends AnimObject{
 
-When displaying .svg, it is assumed that image content is placed
-correctly on a same-sized document as the svg upon which image
-is placed. Position and scaling operates on the placed svg.
+	constructor(params, aoParent){
 
-.png and .jpg images cannot be rescaled currently.
+        // Inherited from AnimObject
+        super(params, aoParent)
 
-// NEEDS TO EXTEND AnimObject BUT DOES NOT YET!
+        // Parameters required by Image but not updated along AnimObject
+        // parameters
+        let relSize = params.relSize
+        let path = params.path
 
-*/
-
-    constructor(params){
-
-        this.pos          = params.pos	
-        this.path         = params.path
-        this.id           = params.id
-        this.relSize      = params.relSize
-        this.relEntPoint  = params.relEntPoint
-        this.moveInEase   = params.moveInEase || d3.easeLinear
-        this.drawType     = params.drawType || "scalein"
-        this.initSize     = (this.drawType == "scalein") ? (params.initSize || 1/4) : null
-		this.parentId        = params.parentId || "bgsvg"
-
-        this.g = d3.select('#'+this.parentId).append('g')
-                    .attr('id', this.id)
-                    .style('opacity', 0.0)
-
-        let imgs = this.g.selectAll("image").data([0])
-
-        if (this.path.endsWith('.svg')){
+        // Initialize the image
+        let imgs = this.aoG.selectAll("image").data([0])
+        if (path.endsWith('.svg')){
             imgs.enter()
                 .append("svg:image")
-                .attr("href", this.path)
-                .attr("x", this.pos[0])
-                .attr("y", this.pos[1])				
-                .attr("width", +this.relSize[0]+"%")
-                .attr("height", +this.relSize[1]+"%")		
-        } else if (this.path.endsWith('.png') | this.path.endsWith('.jpg')){
+                .attr("href", path)
+                .attr("width", + relSize+"%")
+                .attr("height", + relSize+"%")
+        } else if (path.endsWith('.png') | path.endsWith('.jpg')){
             imgs.enter()
                 .append("svg:image")
-                .attr("href", this.path)
-                .attr("x", this.pos[0])
-                .attr("y", this.pos[1])			
+                .attr("href", path)
         }
-    }
-
-    Draw({delay, duration}={}){
-        if (this.drawType==="scalein"){
-            this.g.attr("transform", "translate(" + this.pos[0] + "," + this.pos[1]  + ")" + "scale(" + this.initSize +")")
-            this.g.transition()
-                    .delay(delay)
-                    .duration(duration)  
-                    .style('opacity', 1.0)
-                    .attr("transform", "  scale(" + 1 +")")
-                    .ease(this.moveInEase)
-        } else if (this.drawType==="fadein"){
-            this.g.transition()
-                    .delay(delay)
-                    .duration(duration)  
-                    .style('opacity', 1.0)
-                    .ease(this.moveInEase)
-        } else if (this.drawType == 'movein'){
-            this.g.attr("transform", "translate(" + (this.relEntPoint[0]) + "," + (this.relEntPoint[1]) + ")")
-            this.g.transition()
-                    .delay(delay)
-                    .duration(duration)
-                    .style("opacity",1.0)
-                    .attr("transform", "translate(" + 0 + "," + 0  + ")")
-                    .ease(this.moveInEase)			
-        }
-    }
-
-    Hide({delay, duration}={}){
-        this.g.transition()
-                .delay(delay)
-                .duration(duration)        
-                .style("opacity", 0.0)
-    }
+	}
 }
